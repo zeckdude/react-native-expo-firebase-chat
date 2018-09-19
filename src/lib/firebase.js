0e5677b77a377b6e6e254405d483e3e43f8ce994
Chris Seckler <zeckdude@gmail.com>
2018-09-18 22:43:04 -0700

    Add config hints so its easier to tell whats needed

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,14 @@
 import * as firebase from 'firebase';
-import firebaseConfig from 'config/firebase';
+import myFirebaseConfig from 'config/firebase';
+
+const firebaseConfig = {
+  apiKey: myFirebaseConfig.API_KEY,
+  authDomain: myFirebaseConfig.AUTH_DOMAIN,
+  databaseURL: myFirebaseConfig.DATABASE_URL,
+  projectId: myFirebaseConfig.PROJECT_ID,
+  storageBucket: myFirebaseConfig.STORAGE_BUCKET,
+  messagingSenderId: myFirebaseConfig.MESSAGING_SENDER_ID,
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
